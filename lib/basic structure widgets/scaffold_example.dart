ef2812684e61484aeada5aa053333eab3208b8ad
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-28 07:46:17 +0500

    Scaffold exasmple start

--- lib/basic structure widgets/scaffold_example.dart
+++ lib/basic structure widgets/scaffold_example.dart
@@ -1,3 +1,14 @@
 // Q1: Create a simple Flutter app using MaterialApp and Scaffold.
 // - Add an AppBar with the title "Basic Structure App".
 // - Inside the body, place a Center widget with a Text widget that says "Hello from Scaffold!".
+
+import 'package:flutter/material.dart';
+
+class ScaffoldExample extends StatelessWidget {
+  const ScaffoldExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
