d9991dd825f017fbfd3530d24b646e53b422d32c
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-29 04:40:10 +0500

    Column example done

--- lib/basic structure widgets/column_example.dart
+++ lib/basic structure widgets/column_example.dart
@@ -2,3 +2,25 @@
 // - Create a Scaffold with an AppBar titled "Column Example".
 // - Inside the body, place a Column with mainAxisAlignment set to center.
 // - Add three Text widgets as children: "Item 1", "Item 2", "Item 3".
+
+import 'package:flutter/material.dart';
+
+class ColumnExample extends StatelessWidget {
+  const ColumnExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Column Exmaple')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [Text('Item 1'), Text('Item 2'), Text('Icon 3')],
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(ColumnExample());
