41c84c6fd07c315b1701e992be09df5d4fa60c40
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-29 04:04:43 +0500

    Appbar example done

--- lib/basic structure widgets/appbar_example.dart
+++ lib/basic structure widgets/appbar_example.dart
@@ -3,3 +3,31 @@
 // - Set the AppBar title to "AppBar Example" and center it.
 // - Add an IconButton in the actions (icon: Icons.search).
 // - Inside the body, display a Text widget "AppBar in Action" centered.
+
+import 'package:flutter/material.dart';
+
+class AppbarExample extends StatelessWidget {
+  const AppbarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Appbar Example'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                debugPrint('Search button assign');
+              },
+            ),
+          ],
+        ),
+        body: const Center(child: Text('Appbar in action')),
+      ),
+    );
+  }
+}
