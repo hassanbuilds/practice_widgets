179f2fc0e4f71e6732a738cd08493c6ffef85ed9
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-30 01:38:46 +0500

    question 1 done

--- lib/Inout and interaction widgets/q1_appbar.dart
+++ lib/Inout and interaction widgets/q1_appbar.dart
@@ -2,3 +2,16 @@
 // Requirements:
 // - Use MaterialApp and Scaffold
 // - Add an AppBar with title: "User Registration"
+
+import 'package:flutter/material.dart';
+
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(title: Text('User Registration'))),
+    );
+  }
+}
