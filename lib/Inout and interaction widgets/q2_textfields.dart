ae4228f59d08a55608e7bbe847fc72c5f4d2cc19
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-30 01:47:19 +0500

    q2 done

--- lib/Inout and interaction widgets/q2_textfields.dart
+++ lib/Inout and interaction widgets/q2_textfields.dart
@@ -4,3 +4,31 @@
 //   - Name
 //   - Email
 //   - Password (with obscureText true)
+
+import 'package:flutter/material.dart';
+
+class TextFieldApp extends StatelessWidget {
+  const TextFieldApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: const [
+              TextField(decoration: InputDecoration(labelText: 'Name')),
+              TextField(decoration: InputDecoration(labelText: 'Email')),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
