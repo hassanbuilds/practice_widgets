10ab41fbbf30bbe60ede8e3b6825b75770f5093f
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-30 06:02:24 +0500

    q7  done

--- lib/Inout and interaction widgets/q6_switch.dart
+++ lib/Inout and interaction widgets/q6_switch.dart
@@ -2,3 +2,33 @@
 // Requirements:
 // - SwitchListTile with label: "Enable Notifications"
 // - Print ON/OFF state when toggled
+
+import 'package:flutter/material.dart';
+
+class SwitchApp extends StatefulWidget {
+  const SwitchApp({super.key});
+
+  @override
+  State<SwitchApp> createState() => _SwitchAppState();
+}
+
+class _SwitchAppState extends State<SwitchApp> {
+  bool notification = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: SwitchListTile(
+          title: Text('Enable Notification'),
+          value: notification,
+          onChanged: (value) {
+            setState(() => notification = value);
+            debugPrint('Notification $notification');
+          },
+        ),
+      ),
+    );
+  }
+}
