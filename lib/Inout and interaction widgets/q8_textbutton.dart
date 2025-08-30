868cb63dd9487340688e3a8a5cf36f9a9397d47c
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-31 02:12:01 +0500

    q8 solved

--- lib/Inout and interaction widgets/q8_textbutton.dart
+++ lib/Inout and interaction widgets/q8_textbutton.dart
@@ -2,3 +2,36 @@
 // Requirements:
 // - TextButton with label: "Already have an account? Login"
 // - On press → show SnackBar: "Redirecting to Login..."
+
+import 'package:flutter/material.dart';
+
+class TextbuttonAPP extends StatefulWidget {
+  const TextbuttonAPP({super.key});
+
+  @override
+  State<TextbuttonAPP> createState() => _TextbuttonAPPState();
+}
+
+class _TextbuttonAPPState extends State<TextbuttonAPP> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: Center(
+          child: Builder(
+            builder:
+                (context) => TextButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Redirecting to Login...')),
+                    );
+                  },
+                  child: const Text('Already have an account? Login'),
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+}
