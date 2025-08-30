45306b553329524421276f1f4949e705ca0a623f
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-31 02:19:48 +0500

    q9 solved

--- lib/Inout and interaction widgets/q9_outlinedbutton.dart
+++ lib/Inout and interaction widgets/q9_outlinedbutton.dart
@@ -2,3 +2,38 @@
 // Requirements:
 // - OutlinedButton with label: "Clear Form"
 // - On press → reset all form fields to default values
+
+import 'package:flutter/material.dart';
+
+class OutlineButton extends StatefulWidget {
+  const OutlineButton({super.key});
+
+  @override
+  State<OutlineButton> createState() => _OutlineButtonState();
+}
+
+class _OutlineButtonState extends State<OutlineButton> {
+  String? text = 'Default';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('text'),
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  text = 'Default';
+                });
+              },
+              child: const Text('Clear form'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
