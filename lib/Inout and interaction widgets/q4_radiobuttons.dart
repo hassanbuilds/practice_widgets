3721e6680ca7892342d7b3641717ee91e23997c2
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-30 05:28:01 +0500

    q4 done

--- lib/Inout and interaction widgets/q4_radiobuttons.dart
+++ lib/Inout and interaction widgets/q4_radiobuttons.dart
@@ -2,3 +2,46 @@
 // Requirements:
 // - Add three RadioListTile widgets: Male, Female, Other
 // - Show the selected gender in a Text widget
+
+import 'package:flutter/material.dart';
+
+class RadioAPP extends StatefulWidget {
+  const RadioAPP({super.key});
+
+  @override
+  State<RadioAPP> createState() => _RadioAPPState();
+}
+
+class _RadioAPPState extends State<RadioAPP> {
+  String? gender;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: Column(
+          children: [
+            RadioListTile(
+              title: const Text('MALE'),
+              value: 'Male',
+              groupValue: gender,
+              onChanged: (value) => setState(() => gender = value),
+            ),
+            RadioListTile(
+              title: Text('Female'),
+              value: 'Female',
+              groupValue: gender,
+              onChanged: (value) => setState(() => gender = value),
+            ),
+            RadioListTile(
+              title: Text('Other'),
+              value: 'other',
+              groupValue: gender,
+              onChanged: (value) => setState(() => gender = value),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
