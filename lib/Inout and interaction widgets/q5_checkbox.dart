d64b695a07f8e132d5a5c0e86e41061456378d7e
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-30 05:47:56 +0500

    q5 done

--- lib/Inout and interaction widgets/q5_checkbox.dart
+++ lib/Inout and interaction widgets/q5_checkbox.dart
@@ -2,3 +2,35 @@
 // Requirements:
 // - CheckboxListTile with label: "I agree to the Terms & Conditions"
 // - Print state (checked/unchecked) in console
+
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class CheckboxApp extends StatefulWidget {
+  const CheckboxApp({super.key});
+
+  @override
+  State<CheckboxApp> createState() => _CheckboxAppState();
+}
+
+class _CheckboxAppState extends State<CheckboxApp> {
+  bool agreed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: CheckboxListTile(
+          title: Text('I agreed  the terms and condition'),
+          value: agreed,
+          onChanged: (value) {
+            setState(() => agreed = value!);
+            debugPrint('Agreed $agreed');
+          },
+        ),
+      ),
+    );
+  }
+}
