9e98de324bf5d88575f385867424a0e2b364ac7e
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-31 02:29:23 +0500

    Q10 SOLVED

--- lib/Inout and interaction widgets/q10_gesturedetector.dart
+++ lib/Inout and interaction widgets/q10_gesturedetector.dart
@@ -3,3 +3,36 @@
 // - Add a ❤ (heart) Icon
 // - Wrap with GestureDetector
 // - On double tap → toggle between red (liked) and grey (unliked)
+
+import 'package:flutter/material.dart';
+
+class GestureDetectorApp extends StatefulWidget {
+  const GestureDetectorApp({super.key});
+
+  @override
+  State<GestureDetectorApp> createState() => _GestureDetectorAppState();
+}
+
+class _GestureDetectorAppState extends State<GestureDetectorApp> {
+  bool liked = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('User Registration')),
+        body: Center(
+          child: GestureDetector(
+            onDoubleTap: () {
+              setState(() => liked = !liked);
+            },
+            child: Icon(
+              Icons.favorite,
+              color: liked ? Colors.red : Colors.grey,
+              size: 50,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
