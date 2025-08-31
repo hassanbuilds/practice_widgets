14557f45049f7b8cc2169aecb8676c7296d16684
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-01 03:18:07 +0500

    q1 solved TextPractice

--- lib/Display Widgets/text.dart
+++ lib/Display Widgets/text.dart
@@ -3,3 +3,28 @@
 // - Use MaterialApp and Scaffold
 // - Add an AppBar with title: "Text Example"
 // - Display a Text widget in the center with custom font size, color, and weight
+
+import 'package:flutter/material.dart';
+
+class TextPractice extends StatelessWidget {
+  const TextPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Text Example')),
+        body: const Center(
+          child: Text(
+            'Hello Flutter',
+            style: TextStyle(
+              fontSize: 24, // make text bigger
+              color: Colors.green, // Text color will be green
+              fontWeight: FontWeight.bold, // This will make font bold
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
