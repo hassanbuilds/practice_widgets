38b5f79f18121519b764ae2268c58b534e8f216c
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-01 04:22:56 +0500

    q5 ListView solved

--- lib/Display Widgets/listview.dart
+++ lib/Display Widgets/listview.dart
@@ -3,3 +3,31 @@
 // - Use MaterialApp and Scaffold
 // - Add an AppBar with title: "ListView Example"
 // - Display a vertical ListView with at least 5 ListTile items (each with icon + text)
+
+import 'package:flutter/material.dart';
+
+class ListViewExample extends StatelessWidget {
+  const ListViewExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('ListView Example')),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: const [
+            ListTile(leading: Icon(Icons.home), title: Text('Home')),
+            ListTile(leading: Icon(Icons.person), title: Text('Profile')),
+            ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+            ),
+            ListTile(leading: Icon(Icons.logout), title: Text('LogOut')),
+          ],
+        ),
+      ),
+    );
+  }
+}
