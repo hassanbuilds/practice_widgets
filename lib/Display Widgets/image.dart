79e8c001d0f022dab184e2079f9664b5343694d1
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-01 03:38:37 +0500

    q3 image practice done

--- lib/Display Widgets/image.dart
+++ lib/Display Widgets/image.dart
@@ -3,3 +3,24 @@
 // - Use MaterialApp and Scaffold
 // - Add an AppBar with title: "Image Example"
 // - Display an Image widget (from assets or network) in the body
+
+import 'package:flutter/material.dart';
+
+class AddingImage extends StatelessWidget {
+  const AddingImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text(' Image Example')),
+        body: Image.network(
+          'https://avatars.githubusercontent.com/u/219761786?v=4&size=64',
+          height: 100,
+          width: 100,
+          fit: BoxFit.cover, // Zoom/crops to Fill
+        ),
+      ),
+    );
+  }
+}
