58bbc056336dafb28872080f815ad21c02417b97
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-01 03:05:31 +0500

    adding practice question to each topics

---
+++ lib/Display Widgets/text.dart
@@ -0,0 +1,5 @@
+// Q1: Create a Text Example app
+// Requirements:
+// - Use MaterialApp and Scaffold
+// - Add an AppBar with title: "Text Example"
+// - Display a Text widget in the center with custom font size, color, and weight
