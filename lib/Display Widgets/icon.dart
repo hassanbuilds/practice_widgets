c041cdc1a2ca93ac05f31d5662e47f394266cfe3
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-01 03:25:24 +0500

    Icon Example done

--- lib/Display Widgets/icon.dart
+++ lib/Display Widgets/icon.dart
@@ -3,3 +3,25 @@
 // - Use MaterialApp and Scaffold
 // - Add an AppBar with title: "Icon Example"
 // - Display an Icon widget in the center with custom size and color
+
+import 'package:flutter/material.dart';
+
+class IconExample extends StatelessWidget {
+  const IconExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Icon Example')),
+        body: const Center(
+          child: Icon(
+            Icons.monetization_on_sharp,
+            size: 54,
+            color: Colors.blueGrey,
+          ),
+        ),
+      ),
+    );
+  }
+}
