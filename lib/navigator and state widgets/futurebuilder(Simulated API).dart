03ed331618040bda8b1ffd63945bbef6a21bc5d5
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-02 04:06:32 +0500

    added practice questions for Async Widgets (FutureBuilder & StreamBuilder)

---
+++ lib/navigator and state widgets/futurebuilder(Simulated API).dart
@@ -0,0 +1,6 @@
+// Q6: FutureBuilder Example (Simulated API)
+// Name: futurebuilder_example.dart
+// Requirements:
+// - Use FutureBuilder to simulate fetching data with Future.delayed
+// - While loading → show CircularProgressIndicator
+// - When data loads → display a ListView of items
--- lib/navigator and state widgets/streambuilder(Live Counter).dart
+++ lib/navigator and state widgets/streambuilder(Live Counter).dart
@@ -1,1 +1,7 @@
 
+// Q7: StreamBuilder Example (Live Counter)
+// Name: streambuilder_example.dart
+// Requirements:
+// - Use StreamBuilder with a Stream that emits numbers every second
+// - Display the live counter on the screen
+// - Show CircularProgressIndicator while waiting for first data
