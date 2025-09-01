aa73ce5d2b00e831579002944fc388dc36a0e4c2
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-02 04:05:29 +0500

    State Management (setState, StatefulWidget)  added practice question to realted file

---
+++ lib/navigator and state widgets/counterapp with setstate.dart
@@ -0,0 +1,6 @@
+// Q4: Counter App with setState
+// Name: counter_state.dart
+// Requirements:
+// - Create a StatefulWidget
+// - Show a counter value in the center
+// - Add "+" and "-" buttons to increase/decrease counter using setState
--- lib/navigator and state widgets/dark and light theme toggle.dart
+++ lib/navigator and state widgets/dark and light theme toggle.dart
@@ -1,1 +1,7 @@
 
+// Q5: Dark/Light Theme Toggle
+// Name: theme_toggle.dart
+// Requirements:
+// - Use a StatefulWidget
+// - Add a Switch in AppBar to toggle between Light and Dark Theme
+// - Rebuild UI when theme changes
