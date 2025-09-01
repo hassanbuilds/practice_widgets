304c6635a113531cc0210a1d05e175b18cc2d2bf
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-01 21:11:21 +0500

    unused import removed

--- lib/Display Widgets/gridview.dart
+++ lib/Display Widgets/gridview.dart
@@ -3,3 +3,26 @@
 // - Use MaterialApp and Scaffold
 // - Add an AppBar with title: "GridView Example"
 // - Display a GridView with at least 6 items (each showing an icon and a text label)
+
+
+
+import 'package:flutter/material.dart';
+
+class Gridview extends StatelessWidget {
+  const Gridview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GridView Example'),
+        ),
+        body: Gridview.count(
+          CrossAxisAlignment:sqrt1_2
+          ch
+        ),
+      ),
+    );
+  }
+}
