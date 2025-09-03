2bfd9e1ddba20bdd58de1696b9e754023a2e197c
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-04 04:28:26 +0500

    q3 done

--- lib/styling and theming widgets/textstyle.dart
+++ lib/styling and theming widgets/textstyle.dart
@@ -2,3 +2,49 @@
 // Requirements:
 // - Display 3 Text widgets with different styles (fontSize, fontWeight, color)
 // - Use GoogleFonts or default TextStyle
+
+import 'package:flutter/material.dart';
+
+class TextStyleExmaple extends StatelessWidget {
+  const TextStyleExmaple({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('TextStyle Example')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Hey this is Hassan',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink,
+                ),
+              ),
+              Text(
+                'whats going on??',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.lightGreen,
+                ),
+              ),
+              Text(
+                'Can you tell me what time it is right now?',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
