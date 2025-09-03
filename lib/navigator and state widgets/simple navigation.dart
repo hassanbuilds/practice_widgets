8b6711f961dc25524b47dbf28c7a7f788c036a12
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-03 20:48:31 +0500

    q1 solved simple navigation

--- lib/navigator and state widgets/simple navigation.dart
+++ lib/navigator and state widgets/simple navigation.dart
@@ -1,7 +1,57 @@
-// Q1: Navigation Example App 
-// Requirements: 
-// - Use MaterialApp and Scaffold  
-//- Add a HomeScreen with a button "Go to Details" 
-// - When pressed, navigate to DetailsScreen 
+// Q1: Navigation Example App
+// Requirements:
+// - Use MaterialApp and Scaffold
+//- Add a HomeScreen with a button "Go to Details"
+// - When pressed, navigate to DetailsScreen
 // - Pass a string "Hello from Home!" to DetailsScreen
- // - Show the string in DetailsScreen
+// - Show the string in DetailsScreen
+
+import 'package:flutter/material.dart';
+
+class NavigationExampleApp extends StatelessWidget {
+  const NavigationExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder:
+                    (context) =>
+                        const DetailScreen(message: 'Hello form the home'),
+              ),
+            );
+          },
+          child: const Text('Go to the detail'),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final String message;
+  const DetailScreen({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Details Screen')),
+      body: Center(child: Text(message, style: const TextStyle(fontSize: 20))),
+    );
+  }
+}
