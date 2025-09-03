0021604079c8ad048830d2b271dfa3aea74bcacc
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-04 04:43:25 +0500

    q4 done

--- lib/styling and theming widgets/box_decoration.dart
+++ lib/styling and theming widgets/box_decoration.dart
@@ -3,3 +3,38 @@
 // - Create a Container with BoxDecoration
 // - Add background color, border, and borderRadius
 // - Place a Text widget inside
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class BoxDecorationExample extends StatelessWidget {
+  const BoxDecorationExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('BoxDecorationExample')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.black, // backgroundColor
+                border: Border.all(color: Colors.green, width: 2),
+                borderRadius: BorderRadius.circular(15), // rounded corner
+              ),
+              child: const Text(
+                'BoxDecoration Example!!!!',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
