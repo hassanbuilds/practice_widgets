e8af0a36bbf89b5b5a18ef77dc038de69518f466
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-04 04:14:57 +0500

    correction done

--- lib/styling and theming widgets/textstyle.dart
+++ lib/styling and theming widgets/textstyle.dart
@@ -1,5 +1,4 @@
 // Q3: TextStyle Example
-// Name: textstyle_example.dart
 // Requirements:
 // - Display 3 Text widgets with different styles (fontSize, fontWeight, color)
 // - Use GoogleFonts or default TextStyle
