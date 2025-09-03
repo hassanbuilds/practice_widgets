40141790ab4089491402138eb6a399940eed9862
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-03 05:25:31 +0500

    question change  for the practice

--- lib/navigator and state widgets/simple navigation.dart
+++ lib/navigator and state widgets/simple navigation.dart
@@ -1,6 +1,7 @@
-// Q1: Simple Navigation Example
-// Name: simple_navigation.dart
-// Requirements:
-// - Create two screens: HomeScreen and SecondScreen
-// - Add a button on HomeScreen to navigate to SecondScreen using Navigator.push
-// - On SecondScreen, show a button to go back using Navigator.pop
+// Q1: Navigation Example App 
+// Requirements: 
+// - Use MaterialApp and Scaffold  
+//- Add a HomeScreen with a button "Go to Details" 
+// - When pressed, navigate to DetailsScreen 
+// - Pass a string "Hello from Home!" to DetailsScreen
+ // - Show the string in DetailsScreen
