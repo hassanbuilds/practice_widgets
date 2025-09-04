3d363b7774fec05014c97cb22abe64e797b043f1
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-04 08:40:34 +0500

    q6 solved

--- lib/styling and theming widgets/margin.dart
+++ lib/styling and theming widgets/margin.dart
@@ -1,6 +1,49 @@
 // Q6: Margin Example
-// Name: margin_example.dart
 // Requirements:
 // - Create two Containers with different colors
 // - Apply margin to one Container to separate them
 // - Show difference between margin and padding
+
+import 'package:flutter/material.dart';
+
+class MarginExample extends StatelessWidget {
+  const MarginExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Margin Example')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.all(25),
+              child: const Text(
+                'This has margin',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.black,
+              padding: EdgeInsets.all(25),
+              child: const Text(
+                'This has padding',
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
