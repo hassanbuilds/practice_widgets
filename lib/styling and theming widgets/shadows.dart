37273c9ec778ef2c0cfb3f2845a5262fd9bdf7d0
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-04 21:11:57 +0500

    q8 solved

--- lib/styling and theming widgets/shadows.dart
+++ lib/styling and theming widgets/shadows.dart
@@ -1,6 +1,47 @@
 // Q8: Shadows Example
-// Name: shadow_example.dart
 // Requirements:
 // - Create a Container with BoxDecoration and boxShadow
 // - Add a Text widget inside
 // - Show shadow effect clearly on light background
+
+import 'package:flutter/material.dart';
+
+class ShadowExample extends StatelessWidget {
+  const ShadowExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Shadow Example')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 10,
+                    spreadRadius: 10,
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Shadow Box',
+                  style: TextStyle(color: Colors.blue, fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
