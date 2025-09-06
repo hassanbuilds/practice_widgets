079c88b623a18cc17956f4f08a20a7df8d47d7a1
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-06 23:58:25 +0500

    q2 solved

--- lib/statefull and stateless widgets/statefull_counter_increment.dart
+++ lib/statefull and stateless widgets/statefull_counter_increment.dart
@@ -4,3 +4,43 @@
 // - Add a FloatingActionButton to increment counter
 // - Use setState to update UI
 // - Display counter in the center
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CounterIncrement());
+}
+
+class CounterIncrement extends StatefulWidget {
+  const CounterIncrement({super.key});
+
+  @override
+  State<CounterIncrement> createState() => _CounterIncrementState();
+}
+
+class _CounterIncrementState extends State<CounterIncrement> {
+  int counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stateful Counter Increment'),
+          centerTitle: true,
+        ),
+        body: Text('$counter', style: TextStyle(fontSize: 20)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
