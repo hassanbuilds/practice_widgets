65aa63f192bb8f1ef2ae99ea65d383ab0a26a1e1
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-09 23:32:52 +0500

    part 1 completed of Portrait vs Landscape Layout

--- lib/Portrait vs Landscape Layout widgets/widget_setup.dart
+++ lib/Portrait vs Landscape Layout widgets/widget_setup.dart
@@ -1,3 +1,21 @@
 // Part 1: Widget Setup
 // - Create a StatefulWidget named "ResponsiveLayout2"
 // - Scaffold with AppBar titled "Orientation Layout"
+
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout2 extends StatefulWidget {
+  const ResponsiveLayout2({super.key});
+
+  @override
+  State<ResponsiveLayout2> createState() => _ResponsiveLayout2State();
+}
+
+class _ResponsiveLayout2State extends State<ResponsiveLayout2> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(title: Text('Orientation Layout'))),
+    );
+  }
+}
