9d01333cef95352b6e49b8daae15b3097a246a30
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-10 22:26:37 +0500

    all parts  in one file and other files deleted

--- lib/LayoutBuilder Width-Adaptive Boxes/widget_setup.dart
+++ lib/LayoutBuilder Width-Adaptive Boxes/widget_setup.dart
@@ -1,3 +1,17 @@
 // Part 1: Widget Setup
 // - Create a StatefulWidget named "ResponsiveLayout4"
 // - Scaffold with AppBar titled "Width Adaptive"
+
+
+
+
+// Part 2: Full Screen Container
+// - Container with color: LightGray
+// - Use LayoutBuilder to get maxWidth
+
+
+// Part 3: Row of Boxes
+// - Row with 3 Flexible widgets
+// - Width of each box adapts based on LayoutBuilder constraints
+// - Colors: Red, Orange, Yellow
+// - Center text: "Box 1", "Box 2", "Box 3"
