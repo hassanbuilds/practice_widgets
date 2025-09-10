39530b2f45771d1344bf1fb6f408708df17c4d7f
Muhammad Hassan <mhassan.codes@gmail.com>
2025-09-11 02:14:03 +0500

    q1 solved AnimatedContainer Example

--- lib/animation/animated_container.dart
+++ lib/animation/animated_container.dart
@@ -1,7 +1,63 @@
 // Q1: AnimatedContainer Example
-// Name: animated_container_example.dart
 // Requirements:
 // - Create a StatefulWidget
 // - Show an AnimatedContainer with width and height 100
 // - On button press, change width to 200 and color to green
 // - Animate smoothly over 1 second
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: AnimatedContainerExample(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class AnimatedContainerExample extends StatefulWidget {
+  const AnimatedContainerExample({super.key});
+
+  @override
+  State<AnimatedContainerExample> createState() =>
+      _AnimatedContainerExampleState();
+}
+
+class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
+  double _width = 100;
+  Color _color = Colors.blueGrey;
+
+  void _animatedBox() {
+    setState(() {
+      _width = _width == 100 ? 200 : 100;
+      _color == Colors.blueGrey ? Colors.green : Colors.blueGrey;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('AnimatedContainer Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              width: _width,
+              color: _color,
+              height: 100,
+              duration: Duration(seconds: 1), // Animation Durtaion
+              curve: Curves.easeInOut, // Smoothe animation Curve
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _animatedBox,
+              child: const Text('Animate Container'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
