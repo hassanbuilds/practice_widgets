a4baf587e91413c0d10234a5817291be87271bae
Muhammad Hassan <mhassan.codes@gmail.com>
2025-10-14 02:54:30 +0500

    little changing added

--- lib/animation/animated_opacity2.dart
+++ lib/animation/animated_opacity2.dart
@@ -95,6 +95,7 @@ class _AnimatedOpacityExampleState extends State<AnimatedOpacityExample> {
                 style: const TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
             ),
