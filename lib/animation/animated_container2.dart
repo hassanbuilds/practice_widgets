e0e99cf8cdf189efacab71a3314f18e0ab8cc26a
Muhammad Hassan <mhassan.codes@gmail.com>
2025-10-13 19:43:51 +0500

    solution started

--- lib/animation/animated_container2.dart
+++ lib/animation/animated_container2.dart
@@ -15,3 +15,28 @@
 // - Animate all changes smoothly over 2 seconds using Curves.easeInOutBack
 // - The button text should change to show the current state ("Grow" or "Shrink")
 // - Add spacing and style for clean UI
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Animation()));
+}
+
+class Animation extends StatefulWidget {
+  const Animation({super.key});
+
+  @override
+  State<Animation> createState() => _AnimationState();
+}
+
+class _AnimationState extends State<Animation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedContainer Advance Example'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
